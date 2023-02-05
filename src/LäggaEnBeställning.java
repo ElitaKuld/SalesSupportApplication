66a8d2ae91664d0569d797829576c1150b8535a9
ElitaKuld <elitakuld@gmail.com>
2023-02-05 22:44:04 +0100

    Gjort: While(true) i SäljstödApplikation och delvis Fel input hantering.

--- src/LäggaEnBeställning.java
+++ src/LäggaEnBeställning.java
@@ -3,13 +3,13 @@ import Tabeller.Kund;
 import Tabeller.Sko;
 import Tabeller.Sko_ingår_i_kategori;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class LäggaEnBeställning {
     public static void main(String[] args) throws IOException {
+        final LäggaEnBeställning läggaEnBeställning = new LäggaEnBeställning();
         final Repository repository = new Repository();
         final Scanner scanner = new Scanner(System.in);
         final List<Kund> allCustomersList = repository.getAllCustomers();
@@ -19,14 +19,26 @@ public class LäggaEnBeställning {
 
         System.out.println("Hej och välkommen till vår webbshop!\n" +
                 "För att lägga en beställning vänligen ange ditt namn och efternamn:");
-        final String name = scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Ange ditt lösenord:");
-        final String password = scanner.nextLine();
+        String password = scanner.nextLine();
         System.out.println();
 
-        final Kund customer = allCustomersList.stream().filter(c -> c.getNamn().equalsIgnoreCase(name)).findAny().orElse(null);
+        // Kontrollera att användaren anger sitt namn och lösenord
+        while (name.equals("") || password.equals("")) {
+            System.out.println("Namnet eller/och lösenordet kan inte vara tomt.\n" +
+                    "Vänligen ange ditt namn och efternamn:");
+            name = scanner.nextLine();
+            System.out.println("Ange ditt lösenord:");
+            password = scanner.nextLine();
+            System.out.println();
+        }
+
+        final String finalName = name;
+        final String finalPassword = password;
+        final Kund customer = allCustomersList.stream().filter(c -> c.getNamn().equalsIgnoreCase(finalName)).findAny().orElse(null);
         if (customer != null) {
-            if (customer.getLösenord().equalsIgnoreCase(password)) {
+            if (customer.getLösenord().equalsIgnoreCase(finalPassword)) {
                 System.out.println("Tack " + customer.getNamn() + ". Du kan nu fortsätta med din beställning.");
             } else {
                 System.out.println("Felaktigt lösenord.");
@@ -37,55 +49,19 @@ public class LäggaEnBeställning {
             System.exit(0);
         }
 
-        System.out.println("""
-                Vilken skokategori är du intresserad av? Ange siffran som motsvarar önskade kategorin:
-                Damskor : 1
-                Herrskor : 2
-                Barnskor : 3
-                Sportskor : 4
-                Promenadskor : 5
-                Sandaler : 6""");
-        final String svarKategori = scanner.nextLine().trim();
-        System.out.println();
-        final String category;
-        switch (svarKategori) {
-            case "1" -> {
-                category = "Damskor";
-                System.out.println("Du har valt : Damskor");
-            }
-            case "2" -> {
-                category = "Herrskor";
-                System.out.println("Du har valt : Herrskor");
-            }
-            case "3" -> {
-                category = "Barnskor";
-                System.out.println("Du har valt : Barnskor");
-            }
-            case "4" -> {
-                category = "Sportskor";
-                System.out.println("Du har valt : Sportskor");
-            }
-            case "5" -> {
-                category = "Promenadskor";
-                System.out.println("Du har valt : Promenadskor");
-            }
-            case "6" -> {
-                category = "Sandaler";
-                System.out.println("Du har valt : Sandaler");
-            }
-            default -> {
-                category = null;
-                System.out.println("Okänd kategori");
-            }
+        String category = läggaEnBeställning.printCategoryOptions(scanner);
+        while (category==null){
+            System.out.println("Okänd kategori. Prova gärna igen!");
+            category = läggaEnBeställning.printCategoryOptions(scanner);
         }
+        final String finalCategory = category;
 
         // skapa en lista med alla skor tillhörande någon kategori
         final List<Sko_ingår_i_kategori> allCategoriesWithRespectiveShoesList = repository.getAllCategoriesWithRespectiveShoes();
-        //allCategoriesWithRespectiveShoesList.stream().forEach(s -> System.out.println(s.getSko().getId() + s.getSko().getMärke().getNamn() + s.getKategori().getNamn()));
 
         // välja ut den önskade kategorin med tillhörande skor
         final List<Sko_ingår_i_kategori> relevantCategoryShoes = allCategoriesWithRespectiveShoesList.stream().
-                filter(s -> s.getKategori().getNamn().equalsIgnoreCase(category)).toList();
+                filter(s -> s.getKategori().getNamn().equalsIgnoreCase(finalCategory)).toList();
 
         System.out.println("Vilket av dem nedanstående märkena skulle du vilja beställa?");
         relevantCategoryShoes.stream().map(s -> s.getSko().getMärke().getNamn()).distinct().forEach(System.out::println);
@@ -121,20 +97,26 @@ public class LäggaEnBeställning {
         final String size = scanner.nextLine().trim();
         System.out.println();
 
-        System.out.println("Hur många par av denna sko skulle du vilja beställa?");
-        final int amount = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println();
+        int amount = läggaEnBeställning.printAmountQuestion(scanner);
 
         // peka ut den önskade skon
         final Sko shoeToOrder = relevantColourShoes.stream().filter(s -> s.getSko().getStorlek().equalsIgnoreCase(size)).findAny().get().getSko();
         final int shoeId = shoeToOrder.getId();
         final int customerId = customer.getId();
 
+        // Kontrollera att kunden inte försöker beställa fler skor än det finns i lager
+        while (shoeToOrder.getAntal_i_lager()<amount){
+            System.out.println("Du försöker beställa fler par av denna sko än vi har i lager.\n" +
+                    "Antal i lager: " + shoeToOrder.getAntal_i_lager());
+            amount = läggaEnBeställning.printAmountQuestion(scanner);
+        }
+
+        final int finalAmount = amount;
+
         // kontrollera om kunden redan har en beställning hos oss
         final List<Beställning> allOrdersList = repository.getAllOrders();
 
-        System.out.println("Du ska nu beställa följande sko:\n" + shoeToOrder.getData() + ", antal: " + amount);
+        System.out.println("Du ska nu beställa följande skon:\n" + shoeToOrder.getData() + ", antal: " + finalAmount);
         System.out.println();
 
         // hitta en beställning som tillhör kunden
@@ -151,6 +133,58 @@ public class LäggaEnBeställning {
         } else orderId = 0;
         System.out.println();
 
-        repository.addToCart(customerId, orderId, shoeId, amount);
+        repository.addToCart(customerId, orderId, shoeId, finalAmount);
+    }
+
+    public String printCategoryOptions(Scanner scanner){
+        System.out.println("""
+                Vilken skokategori är du intresserad av? Ange siffran som motsvarar önskade kategorin:
+                Damskor : 1
+                Herrskor : 2
+                Barnskor : 3
+                Sportskor : 4
+                Promenadskor : 5
+                Sandaler : 6""");
+        String svarKategori = scanner.nextLine().trim();
+        System.out.println();
+        String category;
+        switch (svarKategori) {
+            case "1" -> {
+                category = "Damskor";
+                System.out.println("Du har valt : Damskor");
+            }
+            case "2" -> {
+                category = "Herrskor";
+                System.out.println("Du har valt : Herrskor");
+            }
+            case "3" -> {
+                category = "Barnskor";
+                System.out.println("Du har valt : Barnskor");
+            }
+            case "4" -> {
+                category = "Sportskor";
+                System.out.println("Du har valt : Sportskor");
+            }
+            case "5" -> {
+                category = "Promenadskor";
+                System.out.println("Du har valt : Promenadskor");
+            }
+            case "6" -> {
+                category = "Sandaler";
+                System.out.println("Du har valt : Sandaler");
+            }
+            default -> {
+                category = null;
+            }
+        }
+        return category;
+    }
+
+    public int printAmountQuestion(Scanner scanner){
+        System.out.println("Hur många par av denna sko skulle du vilja beställa?");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        return amount;
     }
 }
--- src/SäljstödApplikation.java
+++ src/SäljstödApplikation.java
@@ -27,158 +27,179 @@ public class SäljstödApplikation {
         // allOrdersAndTheirContentList.stream().forEach(b -> System.out.println(b.getData()));
         // System.out.println();
 
-        System.out.println("""
-                Hej och välkommen till vår säljstödsapplikation!
-                Vilken rapport skulle du vilja titta på? Ange siffran som motsvarar önskade rapporten:
-                1 : rapport som listar alla kunder som har handlat varor hos oss.
-                2 : rapport som listar alla kunder och hur många ordrar varje kund har lagt.
-                3 : rapport som listar alla kunder och hur mycket pengar varje kund, sammanlagt, har beställt för.
-                4 : rapport som listar beställningsvärde per ort.
-                5 : topplista över de mest sålda produkterna som listar varje modell och hur många ex som har sålts av den modellen.""");
-        final int reportNumber = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println();
+        System.out.println("Hej och välkommen till vår säljstödsapplikation!");
 
-        // En rapport som listar alla kunder, med namn och adress, som har handlat varor i en viss storlek, av en viss färg eller av ett visst märke.
-        if (reportNumber == 1) {
+        while (true) {
             System.out.println("""
-                    Vilket attribut vill du söka på? Ange:
-                    1 : om du vill söka på färg.
-                    2 : om du vill söka på storlek.
-                    3 : om du vill söka på märke.""");
-            final int answerNumber = scanner.nextInt();
+                    Vilken rapport skulle du vilja titta på? Ange siffran som motsvarar önskade rapporten:
+                    1 : rapport som listar alla kunder som har handlat varor hos oss.
+                    2 : rapport som listar alla kunder och hur många ordrar varje kund har lagt.
+                    3 : rapport som listar alla kunder och hur mycket pengar varje kund, sammanlagt, har beställt för.
+                    4 : rapport som listar beställningsvärde per ort.
+                    5 : topplista över de mest sålda produkterna som listar varje modell och hur många ex som har sålts av den modellen.""");
+
+            int answer = 0;
+            try {
+                answer = scanner.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.println("Invalid input.");
+                System.exit(0);
+            }
             scanner.nextLine();
             System.out.println();
 
-            if (answerNumber == 1) {
-                System.out.println("Vilken färg är du intresserad av: ");
-                final List<String> allColoursList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getFärg).
-                        map(Färg::getNamn).distinct().sorted(collator).toList();
-                allColoursList.forEach(System.out::println);
-                final String colour = scanner.nextLine().trim();
+            final int reportNumber = answer;
+            // En rapport som listar alla kunder, med namn och adress, som har handlat varor i en viss storlek, av en viss färg eller av ett visst märke.
+            if (reportNumber == 1) {
+                System.out.println("""
+                        Vilket attribut vill du söka på? Ange:
+                        1 : om du vill söka på färg.
+                        2 : om du vill söka på storlek.
+                        3 : om du vill söka på märke.""");
+
+                try {
+                    answer = scanner.nextInt();
+                }
+                catch (InputMismatchException e){
+                    System.out.println("Invalid input.");
+                    System.exit(0);
+                }
+                scanner.nextLine();
                 System.out.println();
 
-                if (allColoursList.stream().noneMatch(färg -> färg.equals(colour))) { // opposite to anyMatch
-                    System.out.println("Fel angiven färg");
-                    System.exit(0);
+                final int answerNumber = answer;
+                if (answerNumber == 1) {
+                    System.out.println("Vilken färg är du intresserad av: ");
+                    final List<String> allColoursList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getFärg).
+                            map(Färg::getNamn).distinct().sorted(collator).toList();
+                    allColoursList.forEach(System.out::println);
+                    final String colour = scanner.nextLine().trim();
+                    System.out.println();
+
+                    if (allColoursList.stream().noneMatch(färg -> färg.equals(colour))) { // opposite to anyMatch
+                        System.out.println("Fel angiven färg.");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Nedanstående kunder har handlat varor i följande färg: " + colour);
+                        salesSupportApplication.searchForProduct(colour, salesSupportApplication.lookingForColour, allOrdersAndTheirContentList);
+                    }
+
+
+                } else if (answerNumber == 2) {
+                    System.out.println("Vilken storlek är du intresserad av: ");
+                    final List<String> allSizesList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getStorlek).
+                            distinct().sorted().toList();
+                    allSizesList.forEach(System.out::println);
+                    final String size = scanner.nextLine();
+                    System.out.println();
+
+                    if (allSizesList.stream().noneMatch(storlek -> storlek.equals(size))) { // opposite to anyMatch
+                        System.out.println("Fel angiven storlek.");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Nedanstående kunder har handlat varor i följande storlek: " + size);
+                        salesSupportApplication.searchForProduct(size, salesSupportApplication.lookingForSize, allOrdersAndTheirContentList);
+                    }
+
+                } else if (answerNumber == 3) {
+                    System.out.println("Vilket märke är du intresserad av: ");
+                    final List<String> allBrandsList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getMärke).
+                            map(Märke::getNamn).sorted(collator).distinct().toList();
+                    allBrandsList.forEach(System.out::println);
+                    final String brand = scanner.nextLine();
+                    System.out.println();
+
+                    if (allBrandsList.stream().noneMatch(märke -> märke.equals(brand))) { // opposite to anyMatch
+                        System.out.println("Felt angivet märke.");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Nedanstående kunder har handlat varor av följande märke: " + brand);
+                        salesSupportApplication.searchForProduct(brand, salesSupportApplication.lookingForBrand, allOrdersAndTheirContentList);
+                    }
+
                 } else {
-                    System.out.println("Nedanstående kunder har handlat varor i följande färg: " + colour);
-                    salesSupportApplication.searchForProduct(colour, salesSupportApplication.lookingForColour, allOrdersAndTheirContentList);
+                    System.out.println("Denna angivna siffra motsvarar inte någon sökparameter.");
+                    System.exit(0);
                 }
 
 
-            } else if (answerNumber == 2) {
-                System.out.println("Vilken storlek är du intresserad av: ");
-                final List<String> allSizesList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getStorlek).
-                        distinct().sorted().toList();
-                allSizesList.forEach(System.out::println);
-                final String size = scanner.nextLine();
-                System.out.println();
+                // En rapport som listar alla kunder och hur många ordrar varje kund har lagt. Skriv ut namn och sammanlagda antalet ordrar för varje kund.
+            } else if (reportNumber == 2) {
+                System.out.println("Så många ordrar har varit lagda av varje kund:");
 
-                if (allSizesList.stream().noneMatch(storlek -> storlek.equals(size))) { // opposite to anyMatch
-                    System.out.println("Fel angiven storlek");
-                    System.exit(0);
-                } else {
-                    System.out.println("Nedanstående kunder har handlat varor i följande storlek: " + size);
-                    salesSupportApplication.searchForProduct(size, salesSupportApplication.lookingForSize, allOrdersAndTheirContentList);
-                }
+                final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
+                        map(Beställning_Innehåller::getBeställning).distinct().toList();
 
-            } else if (answerNumber == 3) {
-                System.out.println("Vilket märke är du intresserad av: ");
-                final List<String> allBrandsList = allOrdersAndTheirContentList.stream().map(Beställning_Innehåller::getSko).map(Sko::getMärke).
-                        map(Märke::getNamn).sorted(collator).distinct().toList();
-                allBrandsList.forEach(System.out::println);
-                final String brand = scanner.nextLine();
-                System.out.println();
+                // grouping by customer
+                final Map<Kund, List<Beställning>> groupedByCustomerMap = allOrdersList.stream().collect(Collectors.
+                        groupingBy(Beställning::getKund));
 
-                if (allBrandsList.stream().noneMatch(märke -> märke.equals(brand))) { // opposite to anyMatch
-                    System.out.println("Felt angivet märke");
-                    System.exit(0);
-                } else {
-                    System.out.println("Nedanstående kunder har handlat varor av följande märke: " + brand);
-                    salesSupportApplication.searchForProduct(brand, salesSupportApplication.lookingForBrand, allOrdersAndTheirContentList);
-                }
+                final Collection<List<Beställning>> justOrdersMap = groupedByCustomerMap.values();
+
+                final Map<String, Integer> groupedByCustomerNameMap = justOrdersMap.stream().collect(Collectors.toMap(lista -> lista.get(0).
+                        getKund().getNamn(), lista -> lista.size()));
+
+                // Sorting customers by name
+                final Map<String, Integer> groupedByCustomerNameMapSorted = new TreeMap<>(groupedByCustomerNameMap);
+
+                groupedByCustomerNameMapSorted.forEach((k, v) -> System.out.println(k + " : " + v));
+
+
+                // En rapport som listar alla kunder och hur mycket pengar varje kund, sammanlagt, har beställt för. Skriv ut varje kunds namn och summa.
+            } else if (reportNumber == 3) {
+                System.out.println("Så mycket pengar har varje kund beställt för:");
+
+                final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
+                        map(Beställning_Innehåller::getBeställning).distinct().toList();
+
+                // grouping by customer
+                final Map<Kund, List<Beställning>> groupedByCustomerMap = allOrdersList.stream().collect(Collectors.
+                        groupingBy(Beställning::getKund));
+
+                // creating new map with customer with respective amounts of money spent
+                final Map<String, Double> groupedByCustomerNameMap = new HashMap<>();
+                groupedByCustomerMap.forEach((k, v) -> groupedByCustomerNameMap.put(k.getNamn(), v.stream().mapToDouble(Beställning::getSumma).sum()));
+
+                // Sorting customers by name
+                final Map<String, Double> groupedByCustomerNameMapSorted = new TreeMap<>(groupedByCustomerNameMap);
+
+                groupedByCustomerNameMapSorted.forEach((k, v) -> System.out.println(k + " : " + v));
+
+
+                // En rapport som listar beställningsvärde per ort. Skriv ut orternas namn och summa.
+            } else if (reportNumber == 4) {
+                System.out.println("Så mycket pengar har det blivit spenderat per ort:");
+
+                final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
+                        map(Beställning_Innehåller::getBeställning).distinct().toList();
+
+                // grouping by city
+                final Map<String, List<Beställning>> groupedByCityMap = allOrdersList.stream().collect(Collectors.
+                        groupingBy(beställning -> beställning.getKund().getOrt()));
+
+                // creating new map with cities and how much money was spent
+                final Map<String, Double> groupedByCityMapWithTotals = new HashMap<>();
+                groupedByCityMap.forEach((k, v) -> groupedByCityMapWithTotals.put(k, v.stream().mapToDouble(Beställning::getSumma).sum()));
+
+                // Sorting cities by name
+                final Map<String, Double> groupedByCityMapWithTotalsSorted = new TreeMap<>(groupedByCityMapWithTotals);
+
+                groupedByCityMapWithTotalsSorted.forEach((k, v) -> System.out.println(k + " : " + v));
+
+
+                // En topplista över de mest sålda produkterna som listar varje modell och hur många ex som
+                // har sålts av den modellen. Skriv ut namn på modellen och hur många ex som sålts.
+            } else if (reportNumber == 5) {
+                System.out.println("Topp 10 sålda produkter:");
+                System.out.println("Work in progress...");
+
 
             } else {
-                System.out.println("Denna angivna siffra motsvarar inte någon sökparameter.");
+                System.out.println("Denna angivna siffra motsvarar inte någon rapport.");
                 System.exit(0);
             }
-
-
-            // En rapport som listar alla kunder och hur många ordrar varje kund har lagt. Skriv ut namn och sammanlagda antalet ordrar för varje kund.
-        } else if (reportNumber == 2) {
-            System.out.println("Så många ordrar har varit lagda av varje kund:");
-
-            final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
-                    map(Beställning_Innehåller::getBeställning).distinct().toList();
-
-            // grouping by customer
-            final Map<Kund, List<Beställning>> groupedByCustomerMap = allOrdersList.stream().collect(Collectors.
-                    groupingBy(Beställning::getKund));
-
-            final Collection<List<Beställning>> justOrdersMap = groupedByCustomerMap.values();
-
-            final Map<String, Integer> groupedByCustomerNameMap = justOrdersMap.stream().collect(Collectors.toMap(lista -> lista.get(0).
-                    getKund().getNamn(), lista -> lista.size()));
-
-            // Sorting customers by name
-            final Map<String, Integer> groupedByCustomerNameMapSorted = new TreeMap<>(groupedByCustomerNameMap);
-
-            groupedByCustomerNameMapSorted.forEach((k, v) -> System.out.println(k + " : " + v));
-
-
-            // En rapport som listar alla kunder och hur mycket pengar varje kund, sammanlagt, har beställt för. Skriv ut varje kunds namn och summa.
-        } else if (reportNumber == 3) {
-            System.out.println("Så mycket pengar har varje kund beställt för:");
-
-            final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
-                    map(Beställning_Innehåller::getBeställning).distinct().toList();
-
-            // grouping by customer
-            final Map<Kund, List<Beställning>> groupedByCustomerMap = allOrdersList.stream().collect(Collectors.
-                    groupingBy(Beställning::getKund));
-
-            // creating new map with customer with respective amounts of money spent
-            final Map<String, Double> groupedByCustomerNameMap = new HashMap<>();
-            groupedByCustomerMap.forEach((k, v) -> groupedByCustomerNameMap.put(k.getNamn(), v.stream().mapToDouble(Beställning::getSumma).sum()));
-
-            // Sorting customers by name
-            final Map<String, Double> groupedByCustomerNameMapSorted = new TreeMap<>(groupedByCustomerNameMap);
-
-            groupedByCustomerNameMapSorted.forEach((k, v) -> System.out.println(k + " : " + v));
-
-
-            // En rapport som listar beställningsvärde per ort. Skriv ut orternas namn och summa.
-        } else if (reportNumber == 4) {
-            System.out.println("Så mycket pengar har det blivit spenderat per ort:");
-
-            final List<Beställning> allOrdersList = allOrdersAndTheirContentList.stream().
-                    map(Beställning_Innehåller::getBeställning).distinct().toList();
-
-            // grouping by city
-            final Map<String, List<Beställning>> groupedByCityMap = allOrdersList.stream().collect(Collectors.
-                    groupingBy(beställning -> beställning.getKund().getOrt()));
-
-            // creating new map with cities and how much money was spent
-            final Map<String, Double> groupedByCityMapWithTotals = new HashMap<>();
-            groupedByCityMap.forEach((k, v) -> groupedByCityMapWithTotals.put(k, v.stream().mapToDouble(Beställning::getSumma).sum()));
-
-            // Sorting cities by name
-            final Map<String, Double> groupedByCityMapWithTotalsSorted = new TreeMap<>(groupedByCityMapWithTotals);
-
-            groupedByCityMapWithTotalsSorted.forEach((k, v) -> System.out.println(k + " : " + v));
-
-
-            // En topplista över de mest sålda produkterna som listar varje modell och hur många ex som
-            // har sålts av den modellen. Skriv ut namn på modellen och hur många ex som sålts.
-        } else if (reportNumber == 5) {
-            System.out.println("Topp 10 sålda produkter:");
-
-
-        } else {
-            System.out.println("Denna angivna siffra motsvarar inte någon rapport.");
-            System.exit(0);
+            System.out.println();
         }
-        System.out.println();
     }
 }
